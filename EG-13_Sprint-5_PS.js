93838f8b7844f9902c13fa761ae045e09a2ceadd
khalid66527 <khalidhasan678954321@gmail.com>
2026-09-18 18:45:04 +0600

    added: 2. Binary Search

--- EG-13_Sprint-5_PS.js
+++ EG-13_Sprint-5_PS.js
@@ -1,5 +1,35 @@
 
 
+// 02. Binary Search
+
+var search = function(nums, target) {
+
+    let left = 0;
+    let right = nums.length - 1;
+
+    while (left <= right) {
+
+        let mid = Math.floor((left + right) / 2);
+
+        if (nums[mid] === target) {
+            return mid;
+        }
+
+        if (nums[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+
+    return -1;
+};
+
+console.log(search([-1, 0, 3, 5, 9, 12], 9)); // 4
+
+
+
+
 // 01. Remove Duplicates from Sorted Array
 var removeDuplicates = function(nums) {
     if (nums.length === 0) return 0;
