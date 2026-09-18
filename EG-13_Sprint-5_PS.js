7370d9bdaf55f6ace43de68f605f54891342f2e1
khalid66527 <khalidhasan678954321@gmail.com>
2026-09-18 19:44:27 +0600

    03. Search Insert Position

--- EG-13_Sprint-5_PS.js
+++ EG-13_Sprint-5_PS.js
@@ -1,5 +1,37 @@
 
 
+// 03. Search Insert Position
+
+var searchInsert = function(nums, target) {
+
+    let left = 0;
+    let right = nums.length - 1;
+
+    while (left <= right) {
+
+        let mid = Math.floor((left + right) / 2);
+
+        if (nums[mid] === target) {
+            return mid;
+        }
+
+        if (nums[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+
+    return left;
+};
+
+console.log(searchInsert([1, 3, 5, 6], 5)); // 2
+
+
+
+
+
+
 // 02. Binary Search
 
 var search = function(nums, target) {
@@ -25,7 +57,7 @@ var search = function(nums, target) {
     return -1;
 };
 
-console.log(search([-1, 0, 3, 5, 9, 12], 9)); // 4
+
 
 
 
