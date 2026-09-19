59f77e40c9f8c42876e5c1e86ba49523d252121e
khalid66527 <khalidhasan678954321@gmail.com>
2026-09-19 14:35:14 +0600

    08. Min Stack

--- EG-13_Sprint-5_PS.js
+++ EG-13_Sprint-5_PS.js
@@ -1,4 +1,35 @@
 
+// 08. Min Stack  
+
+var MinStack = function() {
+    this.stack = [];
+    this.minStack = [];
+};
+
+MinStack.prototype.push = function(val) {
+    this.stack.push(val);
+
+    if (!this.minStack.length || val <= this.getMin()) {
+        this.minStack.push(val);
+    }
+};
+
+MinStack.prototype.pop = function() {
+    if (this.stack.pop() === this.getMin()) {
+        this.minStack.pop();
+    }
+};
+
+MinStack.prototype.top = function() {
+    return this.stack[this.stack.length - 1];
+};
+
+MinStack.prototype.getMin = function() {
+    return this.minStack[this.minStack.length - 1];
+};
+
+
+
 
 // 07. Rotate Array
 var rotate = function(nums, k) {
